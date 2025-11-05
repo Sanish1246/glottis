6c6815d1817eb2893859e9866e849ce812bf5522
Sanish1246 <Sanishferrari@gmail.com>
2025-11-05 19:56:41 +0400

    Creating navbar options

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
 import { useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  // const { isLoggedIn, setIsLoggedIn, user, setUser } = useUser();
+
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+
+  const navigate = useNavigate();
+  return (
+    <div>
+      <header>
+        <nav className="flex flex-row justify-between mt-1 border-2 rounded-lg">
+          <h1 className="self-center text-md md:text-2xl font-extrabold">
+            Glottis
+          </h1>
+
+          <div className="flex flex-row gap-4 items-center">
+            <Link to="/">
+              <a className="hover:underline">Home</a>
+            </Link>
+            <Link to="/">
+              <a className="hover:underline">User</a>
+            </Link>
+            <Link to="/">
+              <a className="hover:underline">Lessons</a>
+            </Link>
+            <Link to="/">
+              <a className="hover:underline">Flashcards</a>
+            </Link>
+            <Link to="/">
+              <a className="hover:underline">Deck</a>
+            </Link>
+          </div>
+        </nav>
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import Landing from "./components/pages/Landing.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ index: true, element: <Landing /> }],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
